e2838522eb6eb548ca8299dd330e32e3cce3c5fb
Emanuel Inostroza <einostroza@ubiobio.cl>
2021-07-16 18:28:23 -0400

    config db

--- database/database.js
+++ database/database.js
@@ -1,13 +1,6 @@
 // MySql
 const mysql = require('mysql');
 
-// const conexion = mysql.createConnection({
-//     host: process.env.BD_HOST,
-//     user: process.env.BD_USER,
-//     password: process.env.BD_PASS,
-//     database: process.env.BD_NAME
-// });
-
 const conexion = mysql.createPool({
     host: process.env.BD_HOST,
     user: process.env.BD_USER,
@@ -16,19 +9,5 @@ const conexion = mysql.createPool({
     ssl: false,
     dateStrings: true
 });
-
-// const conexion = function (sql, callback) {
-//     pool.getConnection(function (err, conn) {
-//         if (err) {
-//             callback(err, null);
-//         } else {
-//             conn.query(sql, function (err, results) {
-//                 callback(err, results);
-//             });
-//             conn.release();
-//         }
-//     });
-// };
-
 
 module.exports = conexion;
