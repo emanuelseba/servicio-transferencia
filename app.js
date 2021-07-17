6442f42ad3b5c29e94fa73271bf1849665619e93
Emanuel Inostroza <einostroza@ubiobio.cl>
2021-07-16 20:53:43 -0400

    angular

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
 //Rutas
 const routes = require("./routes/routes");
 
-app.use(express.json());
+//vinvular app angular
+app.use(express.static(path.join(__dirname, 'bin')));
+
+app.use(bodyParser.json());
 app.use(routes)
 
 app.get('/', (req, res) => {
     res.send("Inicio");
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'bin/index.html'));
 });
 
 
